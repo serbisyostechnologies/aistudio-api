1331946599111fd27ecbcff44e546b91b4e6c864
serbisyostechnologies@gmail.com <jobsd@DGBTechDev MINGW64 ~$ git config --global user.name Dareppa>
2026-06-08 10:47:33 +0530

    Vercel mongodb configuration

--- api/index.js
+++ api/index.js
@@ -1,3 +1,15 @@
 import app from "../src/app.js";
+import connectDB from "../src/config/db.connection.js";
 
-export default app;
+let isConnected = false;
+
+const handler = async (req, res) => {
+  if (!isConnected) {
+    await connectDB();
+    isConnected = true;
+  }
+
+  return app(req, res);
+};
+
+export default handler;
--- src/server.js
+++ src/server.js
@@ -2,12 +2,10 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import app from "./app.js";
-import { connectDB } from "./config/db.connection.js";
 
 const PORT = process.env.PORT || 3000;
 const HOST = process.env.HOST || "localhost";
 const startServer = async () => {
-  await connectDB();
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
   });
