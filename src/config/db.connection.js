0465502c7b28fbd55acc2efd5ae1e2e873342d2f
serbisyostechnologies@gmail.com <jobsd@DGBTechDev MINGW64 ~$ git config --global user.name Dareppa>
2026-06-08 10:48:55 +0530

    Avoid reconnecting every request (better pattern)

--- src/config/db.connection.js
+++ src/config/db.connection.js
@@ -2,6 +2,8 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
+    if (mongoose.connection.readyState === 1) return;
+    
     const conn = await mongoose.connect(process.env.MONGO_URI, {
       dbName: process.env.DB_NAME,
       maxPoolSize: 50,
