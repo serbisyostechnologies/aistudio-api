d37296590c875bfc9d6a7a854e7b79f442a4a3cc
serbisyostechnologies@gmail.com <jobsd@DGBTechDev MINGW64 ~$ git config --global user.name Dareppa>
2026-06-08 10:55:33 +0530

    Added export default to connectDb function

--- src/config/db.connection.js
+++ src/config/db.connection.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-export const connectDB = async () => {
+const connectDB = async () => {
   try {
     if (mongoose.connection.readyState === 1) return;
     
@@ -18,3 +18,5 @@ export const connectDB = async () => {
     process.exit(1);
   }
 };
+
+export default connectDB;
